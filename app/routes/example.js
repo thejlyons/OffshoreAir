386a390fade7a050faf3c2acc0a9f70ede183f7b
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2017-11-11 15:15:38 -0700

    added example tab

--- app/routes/example.js
+++ app/routes/example.js
@@ -2,6 +2,6 @@ var example = require('../dummy/example');
 
 module.exports = function(app) {
   app.get('/example', function(req, res) {
-    res.render('pages/example', {data: example.data, title: example.title});
+    res.render('pages/example', {name: example.name, picture: example.picture});
   });
 }
