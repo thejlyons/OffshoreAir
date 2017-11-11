87a121b0560ae1514b86961f52fbd3dddf31dfa4
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2017-11-11 14:14:19 -0700

    changed the navbar, some other things

--- app/routes/login.js
+++ app/routes/login.js
@@ -19,18 +19,18 @@ module.exports = function(app) {
   });
 
   app.post('/login', function(req, res){
-		ACM.manualLogin(req.body['email'], req.body['pass'], function(e, o){
-			if (!o){
-				res.status(400).send(e);
-			}	else{
-				req.session.user = o;
-				if (req.body['remember-me'] == 'true'){
-					res.cookie('email', o.email,    { maxAge: 900000 });
-					res.cookie('pass',  o.password, { maxAge: 900000 });
-				}
-        res.redirect('/employee');
-			}
-		});
+		// ACM.manualLogin(req.body['email'], req.body['pass'], function(e, o){
+		// 	if (!o){
+		// 		res.status(400).send(e);
+		// 	}	else{
+		// 		req.session.user = o;
+		// 		if (req.body['remember-me'] == 'true'){
+		// 			res.cookie('email', o.email,    { maxAge: 900000 });
+		// 			res.cookie('pass',  o.password, { maxAge: 900000 });
+		// 		}
+    //     res.redirect('/employee');
+		// 	}
+		// });
 	});
 
   app.get('/signup', function (req, res) {
