04ee0da78a5826c5b28150e98f6fbbb9e7967fa4
J Lyons <jtlyons@gizmogun.com>
2017-12-07 10:34:25 -0700

    Finished jobs manager

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,5 +1,7 @@
 var formidable = require('formidable');
 var fs = require('fs');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
 var ATM = require('../modules/account-manager');
 var FIM = require('../modules/file-manager');
@@ -16,19 +18,13 @@ module.exports = function(app) {
 
   app.get('/admin/files', function(req, res) {
     if(req.session.user == null || !req.session.user.admin) {
-  		res.redirect('/');
+  		res.redirect('/employee');
   	}	else {
       FIM.getFiles(function(err, files) {
         if(!err) {
           FIM.getLinks(function(err, links) {
             if(!err) {
-              res.render('pages/admin/files', {
-                files: files,
-                links: links,
-                fsapikey: process.env.FILEPICKER_API_KEY,
-                fspolicy: process.env.FILEPICKER_POLICY,
-                fssign: process.env.FILEPICKER_SIGN
-              });
+              res.render('pages/admin/files', {user: req.session.user, files: files, links: links, url: process.env.AWS_BASE_URL});
             }	else {
     					res.render('pages/error', {error: err});
     				}
@@ -40,47 +36,70 @@ module.exports = function(app) {
     }
   });
 
-  app.get('/admin/jobs', function(req, res) {
+  app.post('/admin/manage/links', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
     if(req.session.user == null || !req.session.user.admin) {
-  		res.redirect('/');
-  	}	else {
-      JOM.getJobs(function(err, jobs) {
-        if(!err) {
-          res.render('pages/admin/jobs', {
-            jobs: jobs,
-            fsapikey: process.env.FILEPICKER_API_KEY,
-            fspolicy: process.env.FILEPICKER_POLICY,
-            fssign: process.env.FILEPICKER_SIGN
+      res.send(JSON.stringify({'success': false}));
+    } else {
+      var form = new formidable.IncomingForm();
+      var files = [];
+      var fields = [];
+      var id = null;
+
+      form.multiples = true;
+      form.on('field', function(field, value) {
+        if (field == 'id') {
+          FIM.getLinkByID(value, function(err, link) {
+            if (err) throw err;
+
+            var params = {
+              Bucket: process.env.AWS_BUCKET,
+              Delete: {
+                Objects: [
+                  {
+                    Key: link.link
+                  }
+                ],
+              },
+            };
+
+            s3.deleteObjects(params, function(err, data) {
+              if (err) console.log(err, err.stack); // an error occurred
+              else     console.log(data);           // successful response
+            });
           });
-        }	else {
-					res.render('pages/error', {error: err});
-				}
-      });
-    }
-  });
-
-  app.post('/admin/manage/links/insert', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    if(req.session.user == null || !req.session.user.admin) {
-      res.send(JSON.stringify({'success': false}));
-    } else {
-      FIM.insertLinks(req.body, function(err) {
-        if(err) throw err;
-        res.send(JSON.stringify({'success': true}));
-      });
-    }
-  });
-
-  app.post('/admin/manage/links/delete', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    if(req.session.user == null || !req.session.user.admin) {
-      res.send(JSON.stringify({'success': false}));
-    } else {
-      FIM.deleteLinks(req.body, function(err) {
-        if(err) throw err;
-        res.send(JSON.stringify({'success': true}));
-      });
-      res.send(JSON.stringify({'success': true}));
+          FIM.deleteLink(value);
+        }
+        fields.push([field, value]);
+      })
+        .on('file', function(field, file) {
+          fs.readFile(file.path, function(err, content) {
+            if(!err) {
+              s3.putObject({
+                Bucket: process.env.AWS_BUCKET,
+                Bucket: "offshoreair-uploads",
+                Key: file.name,
+                Body: content,
+                ContentType: file.type,
+                ACL: "public-read"
+              }, function(err, data) {
+                FIM.insertLink(file.name, function(err) {
+                  if(err) {
+                    console.log(err);
+                  }
+                  files.push([field, file]);
+                });
+              });
+            }
+          });
+        })
+        .on('error', function(err) {
+          console.log(err);
+        })
+        .on('end', function() {
+          res.send(JSON.stringify({"success": true}));
+        });
+      form.parse(req);
     }
   });
 
@@ -115,6 +134,25 @@ module.exports = function(app) {
     } else {
       FIM.deleteFiles(req.body.files);
       res.send(JSON.stringify({'success': true}));
+    }
+  });
+
+  app.get('/admin/jobs', function(req, res) {
+    if(req.session.user == null || !req.session.user.admin) {
+  		res.redirect('/');
+  	}	else {
+      JOM.getJobs(function(err, jobs) {
+        if(!err) {
+          res.render('pages/admin/jobs', {
+            jobs: jobs,
+            fsapikey: process.env.FILEPICKER_API_KEY,
+            fspolicy: process.env.FILEPICKER_POLICY,
+            fssign: process.env.FILEPICKER_SIGN
+          });
+        }	else {
+					res.render('pages/error', {error: err});
+				}
+      });
     }
   });
 
