b318f840370cbe6d17e2914bc6109f83d2a02b22
J Lyons <j@jlyons.me>
2021-04-03 16:19:13 -0600

    Require ssl for db connection

--- app/modules/db-connect.js
+++ app/modules/db-connect.js
@@ -5,12 +5,12 @@ const pgp = require('pg-promise')();
 */
 
 var db_url = process.env.DATABASE_URL || "postgres://postgres:killerfly@localhost:5432/postgres";
-db_url = db_url + '?sslmode=require';
+// db_url = db_url + '?sslmode=require';
 
 var conf = {
     connectionString: db_url,
     ssl: {
-      require: true,
+      // require: true,
       rejectUnauthorized: false
     },
 }
